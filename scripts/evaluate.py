63a2d6426fdbcdd84fc66e570d015c471a233fab
Raúl Sanz <raulsanzr@gmail.com>
2025-03-05 09:25:27 +0100

    Implemented plotting of the results

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -1,23 +1,48 @@
-import os
-import sys
 import pandas as pd
-import numpy as np
-from abc import *
+import matplotlib.pyplot as plt
+from simulations import *
+from plot_solution import *
 
-wf_vaf = sim_vafs(final_results['pur_WF'][0], final_results['S_WF'][0], final_results['C_WF'][0], cov, min_reads, xdata, y_prob_wf)
-wf_score = final_results['score_WF'][0]
-exp_vaf = sim_vafs(final_results['pur_EXP'][0], final_results['S_EXP'][0], final_results['C_EXP'][0], cov, min_reads, xdata, y_prob_exp)
-exp_score = final_results['score_EXP'][0]
+# Load data
+out_file = '/home/raul/Documents/projects/TAFI/results/individual/CPCT02020916T.csv'
+donor_id = 'CPCT02020916T'
+solution = pd.read_csv(out_file)
+solution = solution[solution['donor'] == donor_id]
 
-# Creating a 2x2 plot
-fig, axs = plt.subplots(2, 2, figsize=(12, 10))
+donor_bed = pd.read_csv("/home/raul/Documents/projects/TAFI/data/processed/HMF/CPCT02020916T.bed.gz", sep=',', compression='gzip')
 
-wf_hist=plot_histogram(real_vaf, wf_vaf)
-exp_hist=plot_histogram(real_vaf, exp_vaf)
+# Extract Variant Allele Frequency (VAF) and coverage data from the donor's data
+real_vaf = donor_bed['VAF']
+cov = np.array(donor_bed['coverage'])  # Coverage data
+cov_val = np.mean(cov)  # Mean coverage value
+min_reads = donor_bed['AD_ALT'].min()  # Minimum number of reads for the alternate allele
 
-wf_cdf=plot_cdf(real_vaf, wf_vaf)
-exp_cdf=plot_cdf(real_vaf, exp_vaf)
+# Adjust discretization coverage for Wright-Fisher and Exponential models
+discretization_cov = np.array([np.max([int(np.max(cov) * 1.05), 100])])
+lowest_frequency_allowed = 1 / np.max(discretization_cov)
+nr_of_bins = int(1 / lowest_frequency_allowed)
+xdata = np.linspace(lowest_frequency_allowed, 1, nr_of_bins + 1)
 
+# Assuming f_alpha and sim_vafs are defined elsewhere
+y_wf = f_alpha(xdata, 1.0, 1.0)
+y_exp = f_alpha(xdata, 1.0, 2.0)
+y_prob_exp = y_exp / np.sum(y_exp)
+y_prob_wf = y_wf / np.sum(y_wf)
+
+pur_wf, s_wf, c_wf, score_wf = solution['pur_WF'][0], solution['S_WF'][0], solution['C_WF'][0], solution['score_WF'][0]
+pur_exp, s_exp, c_exp, score_exp = solution['pur_EXP'][0], solution['S_EXP'][0], solution['C_EXP'][0], solution['score_EXP'][0]
+
+wf_vaf = sim_vafs(pur_wf, s_wf, c_wf, cov, min_reads, xdata, y_prob_wf)
+exp_vaf = sim_vafs(pur_exp, s_exp, c_exp, cov, min_reads, xdata, y_prob_exp)
+
+fig, axs = plt.subplots(2, 2, figsize=(10, 6))
+# Wright-Fisher model
+plot_histogram(axs[0, 0], real_vaf, wf_vaf, pred_color='tab:green', model='WF')
+plot_cdf(axs[1, 0], real_vaf, wf_vaf, pred_color='tab:green', model='WF')
+axs[0, 0].set_title(f"Wright-Fisher model\npur={pur_wf:.2f}, S={s_wf}, C={c_wf}\nscore={score_wf:.5f}")
+# Exponential model
+plot_histogram(axs[0, 1], real_vaf, exp_vaf, pred_color='tab:red', model='EXP')
+plot_cdf(axs[1, 1], real_vaf, exp_vaf, pred_color='tab:red', model='EXP')
+axs[0, 1].set_title(f"Exponential model\npur={pur_exp:.2f}, S={s_exp}, C={c_exp}\nscore={score_exp:.5f}")
 plt.tight_layout()
-plt.savefig(results_dir + 'plots/' + donor_id + '.png')
-plt.show()
+plt.savefig(f'../results/plots/{donor_id}.png')
---
+++ scripts/plot_solution.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_cdf(ax, real_vaf, pred_vaf, pred_color, model):
+    real_sorted = np.sort(real_vaf)
+    pred_sorted = np.sort(pred_vaf)
+
+    real_cdf = np.arange(1, len(real_sorted) + 1) / len(real_sorted)
+    pred_cdf = np.arange(1, len(pred_sorted) + 1) / len(pred_sorted)
+
+    ax.plot(real_sorted, real_cdf, label='Observed', linewidth=2,  color='tab:blue')
+    ax.plot(pred_sorted, pred_cdf, label='Prediction', linewidth=2, linestyle='--', color=pred_color)
+    ax.set_xlabel('Allele Frequency')
+    if model == 'WF':
+        ax.set_ylabel('Cumulative Probability')
+
+def plot_histogram(ax, real_vaf, pred_vaf, pred_color, model):
+    ax.hist(real_vaf, bins=50, alpha=0.5, range=(0,1), label='Observed', density=True, color='tab:blue')
+    ax.hist(pred_vaf, bins=50, alpha=0.5, range=(0,1), label=f'{model} solution', density=True, color=pred_color)
+    if model == 'WF':
+        ax.set_ylabel('Frequency')
+    ax.legend()
